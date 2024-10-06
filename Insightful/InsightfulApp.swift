64f6603bfc15a9478e53fdb346c06cf4e1ed3400
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-10-06 00:32:21 -0300

    🎉 initial commit!

---
+++ Insightful/InsightfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightfulApp.swift
+//  Insightful
+//
+//  Created by Felipe Pizelli on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct InsightfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
