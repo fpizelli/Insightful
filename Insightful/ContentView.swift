065fdc3878c7d479b75b1536086f60b3cebaf5c2
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-10-06 13:08:55 -0300

    💄 feat:  more view styling

--- Insightful/ContentView.swift
+++ Insightful/ContentView.swift
@@ -9,19 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            Text("\u{201c}")
+                .font(.custom("HelveticaNeue-CondensedBlack", size: 90))
+                .padding(.top, -20)
+                .padding(.bottom, 10)
             Spacer()
             
-            Text("Hello!")
-                .font(.title.bold())
+            Text("Lorem ipsum dolor, dolor lorem ipsum! Lorem ipsum dolor!")
+                .font(.system(
+                    size: 32,
+                    weight: .light,
+                    design: .default))
+                .lineSpacing(5)
+                .padding(.bottom, 20)
                 
-            Text("Another hello!")
-                .font(.subheadline)
+            Text("another hello!")
+                .font(.system(
+                    size: 20,
+                    weight: .bold,
+                    design: .default))
+                .foregroundStyle(.primary.opacity(0.7))
             
+            Spacer()
+            Spacer()
             Spacer()
             
             HStack {
-                Button("Tap for more", action: { })
+                Button("tap for more", action: { })
+                    .foregroundStyle(.primary)
+                    .font(.title2)
                 
                 Spacer()
                 
@@ -29,15 +46,19 @@ struct ContentView: View {
                     Image(systemName: "bookmark")
                 }
                 .accessibilityLabel("Bookmark quote")
-                
-                
+                .foregroundStyle(.primary)
+                .font(.title2)
+                                
                 Button(action: { }) {
                     Image(systemName: "square.and.arrow.up")
                 }
                 .accessibilityLabel("Share quote")
+                .foregroundStyle(.primary)
+                .font(.title2)
             }
-            .padding()
         }
+        .padding(.vertical, 10)
+        .padding(.horizontal, 30)
     }
 }
 
